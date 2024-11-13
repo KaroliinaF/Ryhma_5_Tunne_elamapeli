abaa8d635845b50ab19a6b9a340ace1924a9b82a
joqquli <91956751+joqquli@users.noreply.github.com>
2024-11-13 12:25:44 +0200

    script
    Tänne myös jokaisen oma script jatkossa eli esim. jos sinulla on viha niin tänne se sitten nimellä angry. Nyt esim. merkit sivu on täälä token. Silloin pysymme kärryillä mikä on minkäkin sivun js tiedosto.

---
+++ script/token.js
@@ -0,0 +1,9 @@
+function navigate(page) {
+  if (page === 'index') {
+    window.location.href = 'index.html';
+  } else if (page === 'token') {  
+    window.location.href = 'token.html';
+  } else if (page === 'info') {
+    window.location.href = 'info.html';
+  }
+}
